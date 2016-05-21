7ee81962e218d7b8c76953aab100d5edcb0f0f1b
MinskyNg <minsky199525@gmail.com>
2016-05-22 00:07:46 +0800

    增加浮动工具栏

--- src/js/markeditor.js
+++ src/js/markeditor.js
@@ -5,7 +5,7 @@
 // 标题输入区
 var inputTitle = document.getElementById('input-title');
 // 编辑区
-var editor = document.getElementById('code');
+var editor = document.getElementById('editor');
 var $editor = $(editor);
 // 预览区
 var preview = document.getElementById('preview');
@@ -33,13 +33,24 @@ var redoLog = [];
 var $showDouble = $('.show-double');
 var $showEditor = $('.show-editor');
 var $showPreview = $('.show-preview');
+// 浮动工具栏
+var $toolbar = $('.toolbar');
+// 弹出层
+var $formWrapper = $('.form-wrapper');
 // 文章列表
 var $articleList = $('.article-list');
+
+
 // 备用文章
-articleSpare = JSON.stringify([
-        { title: '文章1', body: '# 文章1', date: '2016-5-20 20:41:48' },
-        { title: '文章2', body: '# 文章2', date: '2016-5-20 20:41:49' }]
-);
+articleSpare = JSON.stringify([{
+    title: '文章1',
+    body: '# 文章1',
+    date: '2016-5-20 20:41:48'
+}, {
+    title: '文章2',
+    body: '# 文章2',
+    date: '2016-5-20 20:41:49'
+}]);
 // 获取本地存储文章
 var articleStorage = localStorage.getItem('article') || articleSpare;
 var articleSource = JSON.parse(articleStorage);
@@ -55,10 +66,10 @@ preview.innerHTML = md.render(editor.value);
 
 
 // 将文章保存到本地
-var  saveArticle = function() {
+var saveArticle = function() {
     articleStorage = JSON.stringify(articleSource);
     localStorage.setItem('article', articleStorage);
-}
+};
 
 
 // 动态设置主区域高度
@@ -79,7 +90,7 @@ $editor.on('input propertychange', function() {
 
 
 // 编辑区和预览区设置同步滚动
-var $divs = $('#code, #preview');
+var $divs = $('#editor, #preview');
 var handleScroll = function() {
     // 移除另一个区域的滚动事件 防止循环滚动
     var $other = $divs.not(this).off('scroll');
@@ -97,31 +108,31 @@ $divs.on('scroll', handleScroll);
 
 // 记录标题变动
 inputTitle.addEventListener('change', function() {
+    // 去除前后空格并做XSS输入过滤
+    article.title = inputTitle.value.replace(/(^\s*)|(\s*$)/g, '').replace(/[<>'&]/g, function(match) {
+        switch (match) {
+            case '<':
+                return '&lt;';
+            case '>':
+                return '&gt;';
+            case '&':
+                return '&amp;';
+            case '\'':
+                return '&quot;';
+        }
+    });
     // 若输入为空则取日期为标题
-    article.title = inputTitle.value || article.date;
-    // XSS输入过滤
-    article.title = article.title.replace(/[<>'&]/g, function(match) {
-            switch (match) {
-                case '<':
-                    return '&lt;';
-                case '>':
-                    return '&gt;';
-                case '&':
-                    return '&amp;';
-                case '\'':
-                    return '&quot;';
-         }
-     });
+    article.title = article.title || article.date;
     // 长标题截短
     var title = article.title;
-    title  = (title.length < 11) ? title : title.substring(0, 10) + '...';
+    title = (title.length < 11) ? title : title.substring(0, 10) + '...';
     $articleList.find('li[alt="' + article.date + '"]').html(title + '<a class="delete" title="删除文章"></a>');
     saveArticle();
 });
 
 
 // 定时记录编辑区并自动保存
-function setLog() {
+var setLog = function() {
     if (undoLog[undoLog.length - 1] !== editor.value) {
         // 防止存储数据栈过深
         if (undoLog.length > 50) {
@@ -133,12 +144,29 @@ function setLog() {
         localStorage.setItem('article', articleStorage);
     }
     timer = setTimeout(setLog, 800);
-}
+};
 setLog();
 
 
+// 阻止tab键切换文本框
+editor.addEventListener('keydown', function(event) {
+    var keyCode = event.keyCode || event.which;
+    // 如果按下tab键
+    if (keyCode === 9) {
+        var start = editor.selectionStart,
+            end = editor.selectionEnd;
+        editor.value = editor.value.substring(0, start) + '\t' + editor.value.substring(end);
+        editor.selectionStart = editor.selectionEnd = start + 1;
+        // 阻止默认事件
+        event.preventDefault();
+        $undo.attr('disabled', false).css('background-color', '#fcfcfc');
+        preview.innerHTML = md.render(editor.value);
+    }
+});
+
+
 /*
-工具栏
+导航栏
 */
 
 // 新建文章
@@ -150,7 +178,11 @@ $('.new-article').click(function() {
     redoLog = [];
     $undo.attr('disabled', true).css('background-color', '#555');
     $redo.attr('disabled', true).css('background-color', '#555');
-    article = { title: '新建文章', body: '', date: '' };
+    article = {
+        title: '新建文章',
+        body: '',
+        date: ''
+    };
     articleSource.push(article);
     // 设置日期
     var now = new Date();
@@ -177,10 +209,12 @@ $('.del-article').click(function() {
     }
 });
 
-// 保存文章到本地
+// 下载文章
 $('.down-article').click(function() {
     var name = article.title + '.md';
-    var blob = new Blob([article.body], {type: 'text/plain'});
+    var blob = new Blob([article.body], {
+        type: 'text/plain'
+    });
     if (window.saveAs) {
         // IE
         window.saveAs(blob, name);
@@ -188,12 +222,12 @@ $('.down-article').click(function() {
         // IE
         navigator.saveBlob(blob, name);
     } else {
-        // Chrome Firefox
+        // 非IE
         url = URL.createObjectURL(blob);
         var link = document.createElement("a");
         link.setAttribute('href', url);
         link.setAttribute('download', name);
-        // 点击下载事件模拟
+        // 模拟点击下载事件
         var event = document.createEvent('MouseEvents');
         event.initMouseEvent('click', true, true, window, 1, 0, 0, 0, 0, false, false, false, false, 0, null);
         link.dispatchEvent(event);
@@ -231,8 +265,15 @@ $redo.click(function() {
 
 // 双屏显示
 $showDouble.click(function() {
-    $editor.css({'display': 'block', 'width': '48%'});
-    $preview.css({'display': 'block', 'width': '52%'});
+    $editor.css({
+        'display': 'block',
+        'width': '48%'
+    });
+    $preview.css({
+        'display': 'block',
+        'width': '52%'
+    });
+    $toolbar.css('display', 'block');
     $showDouble.attr('disabled', true).css('background-color', '#555');
     $showEditor.attr('disabled', false).css('background-color', '#fcfcfc');
     $showPreview.attr('disabled', false).css('background-color', '#fcfcfc');
@@ -240,8 +281,15 @@ $showDouble.click(function() {
 
 // 只显示编辑区
 $showEditor.click(function() {
-    $editor.css({'display': 'block', 'width': '100%'});
-    $preview.css({'display': 'none', 'width': '0%'});
+    $editor.css({
+        'display': 'block',
+        'width': '100%'
+    });
+    $preview.css({
+        'display': 'none',
+        'width': '0%'
+    });
+    $toolbar.css('display', 'block');
     $showEditor.attr('disabled', true).css('background-color', '#555');
     $showDouble.attr('disabled', false).css('background-color', '#fcfcfc');
     $showPreview.attr('disabled', false).css('background-color', '#fcfcfc');
@@ -249,8 +297,15 @@ $showEditor.click(function() {
 
 // 只显示预览区
 $showPreview.click(function() {
-    $editor.css({'display': 'none', 'width': '0%'});
-    $preview.css({'display': 'block', 'width': '100%'});
+    $editor.css({
+        'display': 'none',
+        'width': '0%'
+    });
+    $preview.css({
+        'display': 'block',
+        'width': '100%'
+    });
+    $toolbar.css('display', 'none');
     $showPreview.attr('disabled', true).css('background-color', '#555');
     $showEditor.attr('disabled', false).css('background-color', '#fcfcfc');
     $showDouble.attr('disabled', false).css('background-color', '#fcfcfc');
@@ -284,6 +339,123 @@ document.addEventListener('drop', function(event) {
 
 
 /*
+浮动工具栏
+*/
+
+// 在光标处插入文本
+var insertText = function(text) {
+    // IE
+    if (document.selection) {
+        var sel = document.selection.createRange();
+        sel.text = text;
+        // 非IE
+    } else if (typeof editor.selectionStart === 'number' && typeof editor.selectionEnd === 'number') {
+        // 获取文本起始点和结束点
+        var startPos = editor.selectionStart;
+        var endPos = editor.selectionEnd;
+        var cursorPos = startPos;
+        // 插入文本
+        var tmpText = editor.value;
+        editor.value = tmpText.substring(0, startPos) + text + tmpText.substring(endPos, tmpText.length);
+        // 移动光标
+        cursorPos += text.length;
+        editor.selectionStart = editor.selectionEnd = cursorPos;
+    } else {
+        editor.value += text;
+    }
+};
+
+// 获取链接和图片地址
+var getAddress = function(type) {
+    // 弹出表单
+    $formWrapper.css('display', 'block');
+    // 表单点击事件
+    $formWrapper.click(function(event) {
+        var $target = $(event.target);
+        if ($target.is('.cancel')) {
+            $('.form-wrapper input').val('');
+            $formWrapper.css('display', 'none');
+            $formWrapper.off('click keydown');
+        } else if ($target.is('.submit')) {
+            var word = $('.input-word').val().replace(/(^\s*)|(\s*$)/g, '');
+            var address = $('.input-address').val().replace(/(^\s*)|(\s*$)/g, '');
+            if (type === 'img') {
+                insertText('![' + word + ']' + '(' + address + ')');
+            } else {
+                insertText('[' + word + ']' + '(' + address + ')');
+            }
+            $('.form-wrapper input').val('');
+            $formWrapper.css('display', 'none');
+            $formWrapper.off('click keydown');
+        }
+        $undo.attr('disabled', false).css('background-color', '#fcfcfc');
+        preview.innerHTML = md.render(editor.value);
+        event.preventDefault();
+    });
+
+    // 回车提交事件
+    $formWrapper.keydown(function(event) {
+        var keyCode = event.keyCode || event.which;
+        // 如果按下回车键
+        if (keyCode === 13) {
+            var word = $('.input-word').val().replace(/(^\s*)|(\s*$)/g, '');
+            var address = $('.input-address').val().replace(/(^\s*)|(\s*$)/g, '');
+            if (type === 'img') {
+                insertText('![' + word + ']' + '(' + address + ')');
+            } else {
+                insertText('[' + word + ']' + '(' + address + ')');
+            }
+            $('.form-wrapper input').val('');
+            $formWrapper.css('display', 'none');
+            $formWrapper.off('click keydown');
+            preview.innerHTML = md.render(editor.value);
+            event.preventDefault();
+        }
+    });
+};
+
+
+// 根据不同按钮向textarea插入文本
+$toolbar.click(function(event) {
+    var $target = $(event.target);
+    switch ($target.attr('class')) {
+        case 'add-heading':
+            insertText('## 标题');
+            break;
+        case 'add-strong':
+            insertText('**粗体**');
+            break;
+        case 'add-italic':
+            insertText('*斜体*');
+            break;
+        case 'add-code':
+            insertText('\t输入代码');
+            break;
+        case 'add-link':
+            getAddress('link');
+            break;
+        case 'add-img':
+            getAddress('img');
+            break;
+        case 'add-ul':
+            insertText('* 列表项');
+            break;
+        case 'add-ol':
+            insertText('1. 列表项');
+            break;
+        case 'add-quote':
+            insertText('> 块引用');
+            break;
+        case 'add-line':
+            insertText('----------');
+            break;
+    }
+    $undo.attr('disabled', false).css('background-color', '#fcfcfc');
+    preview.innerHTML = md.render(editor.value);
+});
+
+
+/*
 侧边栏
 */
 
@@ -293,7 +465,7 @@ document.addEventListener('drop', function(event) {
         var articleItem = articleSource[i];
         // 长标题截短
         var title = articleItem.title;
-        title  = (title.length < 11) ? title : title.substring(0, 10) + '...';
+        title = (title.length < 11) ? title : title.substring(0, 10) + '...';
         $articleList.append('<li alt="' + articleItem.date + '" title="选择文章">' + title + '<a class="delete" title="删除文章"></a></li>');
     }
     $('.article-list li:first-child').addClass('active');
